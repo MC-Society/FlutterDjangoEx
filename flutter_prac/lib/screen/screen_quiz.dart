4f1ee8a7c0d64549763a7e3ec591e01208398785
wnstjd9288 <wnstjd9288@naver.com>
2024-12-08 14:25:26 +0900

    feat : Swiper로 문제 내용 구현

--- flutter_prac/lib/screen/screen_quiz.dart
+++ flutter_prac/lib/screen/screen_quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_prac/model/model_quiz.dart';
+import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 
 class QuizScreen extends StatefulWidget {
   List<Quiz> quizs;
@@ -28,6 +29,43 @@ class _QuizScreenState extends State<QuizScreen> {
                   border: Border.all(color: Colors.deepPurple)),
               width: width * 0.85,
               height: height * 0.5,
+              child: Swiper(
+                physics: NeverScrollableScrollPhysics(),
+                loop: false,
+                itemCount: widget.quizs.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return _buildQuizCard(
+                      widget.quizs[index], width, height, index + 1);
+                },
+              ),
             ))));
+  }
+
+  Widget _buildQuizCard(
+      Quiz quiz, double width, double height, int _currentIndex) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Colors.white),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(0, width * 0.024, 0, width * 0.024),
+            child: Text(
+              'Q' + _currentIndex.toString() + '.',
+              style: TextStyle(
+                  fontSize: width * 0.06, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            width: width * 0.8,
+            padding: EdgeInsets.only(top: width * 0.012),
+            child: Text(quiz.title),
+          )
+        ],
+      ),
+    );
   }
 }
