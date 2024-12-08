eb22d4be16be36e0eb83cbdc8fec5bb881938659
als8921 <als8921@naver.com>
2024-12-08 16:40:10 +0900

    chore :  quiz 앱 Settings

--- django_prac/django_server/settings.py
+++ django_prac/django_server/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-+4i#!r4i-&h#gxm&io&lm15jf@128r@$k&-y^d-1w(1&^c_(7m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "blog",
     "single_pages",
-    "quiz"
+    "quiz",
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
