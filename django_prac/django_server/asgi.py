5b6301f72c2fa2fb46a04f89843fee7297db9e63
als8921 <als8921@naver.com>
2024-12-08 14:18:12 +0900

    chore :  장고 프로젝트 위치 변경

--- django_prac/django_server/asgi.py
+++ django_prac/django_server/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_prac project.
+ASGI config for django_server project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_prac.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_server.settings")
 
 application = get_asgi_application()
