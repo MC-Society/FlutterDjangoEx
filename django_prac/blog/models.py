2c0f9e5b0910ac1d76125ed575b96c990be55c72
als8921 <als8921@naver.com>
2024-12-08 15:10:54 +0900

    chore :  Post 제목 변경하기

--- django_prac/blog/models.py
+++ django_prac/blog/models.py
@@ -5,3 +5,8 @@ class Post(models.Model):
     content = models.TextField()
 
     created_at = models.DateTimeField()
+
+    # Post의 제목 설정하기 (ADMIN 창에서 보임)
+    # PK : Primary key
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
