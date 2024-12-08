ff2c72ca9476e5981f39faa2e7285a70de09a624
wnstjd9288 <wnstjd9288@naver.com>
2024-12-08 16:08:18 +0900

    feat : quiz 선지 선택 및 다음 문제 기능 추가

--- flutter_prac/lib/screen/screen_quiz.dart
+++ flutter_prac/lib/screen/screen_quiz.dart
@@ -1,5 +1,7 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_prac/model/model_quiz.dart';
+import 'package:flutter_prac/widget/widget_candidate.dart';
 import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 
 class QuizScreen extends StatefulWidget {
@@ -13,6 +15,8 @@ class QuizScreen extends StatefulWidget {
 class _QuizScreenState extends State<QuizScreen> {
   List<int> _answers = [-1, -1, -1];
   List<bool> _answerState = [false, false, false, false];
+  int _currentIndex = 0;
+  SwiperController _controller = SwiperController();
 
   @override
   Widget build(BuildContext context) {
@@ -30,23 +34,23 @@ class _QuizScreenState extends State<QuizScreen> {
               width: width * 0.85,
               height: height * 0.5,
               child: Swiper(
+                controller: _controller,
                 physics: NeverScrollableScrollPhysics(),
                 loop: false,
                 itemCount: widget.quizs.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return _buildQuizCard(
-                      widget.quizs[index], width, height, index + 1);
+                  return _buildQuizCard(widget.quizs[index], width, height);
                 },
               ),
             ))));
   }
 
-  Widget _buildQuizCard(
-      Quiz quiz, double width, double height, int _currentIndex) {
+  Widget _buildQuizCard(Quiz quiz, double width, double height) {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
         border: Border.all(color: Colors.white),
+        color: Colors.white,
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -54,7 +58,7 @@ class _QuizScreenState extends State<QuizScreen> {
           Container(
             padding: EdgeInsets.fromLTRB(0, width * 0.024, 0, width * 0.024),
             child: Text(
-              'Q' + _currentIndex.toString() + '.',
+              'Q' + (_currentIndex + 1).toString() + '.',
               style: TextStyle(
                   fontSize: width * 0.06, fontWeight: FontWeight.bold),
             ),
@@ -62,10 +66,79 @@ class _QuizScreenState extends State<QuizScreen> {
           Container(
             width: width * 0.8,
             padding: EdgeInsets.only(top: width * 0.012),
-            child: Text(quiz.title),
+            child: AutoSizeText(
+              quiz.title,
+              textAlign: TextAlign.center,
+              maxLines: 2,
+              style: TextStyle(
+                  fontSize: width * 0.048, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: Container(),
+          ), // 이후 추가되는 위젯들을 아래쪽부터 배치되게 하는 효과
+          Column(
+            children: _buildCandidates(width, quiz),
+          ),
+          Container(
+            padding: EdgeInsets.all(width * 0.024),
+            child: Center(
+              child: ButtonTheme(
+                minWidth: width * 0.5,
+                height: height * 0.05,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                child: ElevatedButton(
+                  onPressed: _answers[_currentIndex] == -1
+                      ? null
+                      : () {
+                          if (_currentIndex == widget.quizs.length - 1) {
+                          } else {
+                            _answerState = [false, false, false, false];
+                            _currentIndex += 1;
+                            _controller.next();
+                          }
+                        },
+                  child: _currentIndex == widget.quizs.length - 1
+                      ? Text('결과보기', style: TextStyle(color: Colors.white))
+                      : Text('다음문제', style: TextStyle(color: Colors.white)),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurple,
+                      foregroundColor: Colors.white),
+                ),
+              ),
+            ),
           )
         ],
       ),
     );
+  }
+
+  List<Widget> _buildCandidates(double width, Quiz quiz) {
+    List<Widget> _children = [];
+    for (int i = 0; i < 4; i++) {
+      print(i);
+      _children.add(CandWidget(
+          index: i,
+          text: quiz.candidates[i],
+          width: width,
+          answerState: _answerState[i],
+          tap: () {
+            setState(() {
+              for (int j = 0; j < 4; j++) {
+                if (i == j) {
+                  _answerState[j] = true;
+                  _answers[_currentIndex] = j;
+                } else {
+                  _answerState[j] = false;
+                }
+                print(j);
+                print(_answerState[j]);
+              }
+            });
+          }));
+      _children.add(Padding(padding: EdgeInsets.all(width * 0.024)));
+    }
+    return _children;
   }
 }
