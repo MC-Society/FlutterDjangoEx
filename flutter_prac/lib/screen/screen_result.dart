9acdfd4e4b31740e3a25e613cc678ac558b37ae7
wnstjd9288 <wnstjd9288@naver.com>
2024-12-08 16:54:31 +0900

    feat : 결과 확인 및 메인으로 돌아는 기능 추가

--- flutter_prac/lib/screen/screen_result.dart
+++ flutter_prac/lib/screen/screen_result.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_prac/model/model_quiz.dart';
+import 'package:flutter_prac/screen/screen_home.dart';
 
 class ResultScreen extends StatelessWidget {
   List<int> answers;
@@ -33,7 +34,69 @@ class ResultScreen extends StatelessWidget {
           width: width * 0.85,
           height: height * 0.5,
           child: Column(
-            children: <Widget>[],
+            children: <Widget>[
+              Padding(padding: EdgeInsets.only(bottom: width * 0.048)),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Colors.deepPurple),
+                  color: Colors.white,
+                ),
+                width: width * 0.73,
+                height: height * 0.25,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.fromLTRB(0, width * 0.048, 0, 0),
+                      child: Text(
+                        '수고하셨습니다!',
+                        style: TextStyle(
+                            fontSize: width * 0.055,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text(
+                      '당신의 점수는',
+                      style: TextStyle(
+                          fontSize: width * 0.048, fontWeight: FontWeight.bold),
+                    ),
+                    Expanded(child: Container()),
+                    Text(
+                      score.toString() + '/' + quizs.length.toString(),
+                      style: TextStyle(
+                          fontSize: width * 0.2,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(child: Container()),
+              Container(
+                padding: EdgeInsets.only(bottom: width * 0.048),
+                child: ButtonTheme(
+                  minWidth: width * 0.73,
+                  height: height * 0.05,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return HomeScreen();
+                      }));
+                    },
+                    child: Text(
+                      '홈으로 돌아가기',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.deepPurple),
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ),
